a239055bd45b1e45758351085b5cdbcf25504a23
FreewayPvP <34444618+FreewayPvP@users.noreply.github.com>
2018-07-16 12:15:17 +0200

    Yee

---
+++ Startup.bat
@@ -0,0 +1,12 @@
+@echo off
+color 9a
+:Start2
+cls
+goto Start
+:Start
+cd D:\Discord BOTS\Meme Lord
+echo -------------------------------------------------
+echo -------------------Starting Bot------------------
+echo -------------------Please Wait-------------------
+echo -------------------------------------------------
+node app.js
